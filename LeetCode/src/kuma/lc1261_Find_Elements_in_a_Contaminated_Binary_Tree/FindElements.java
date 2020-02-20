1a2bd5767488879d6e10997f4602c6af1026b98e
xiong_yi <xiong_yi@192.168.128.101>
2020-02-20 18:05:51 +0900

    leetcode 1261 Find Elements in a Contaminated Binary Tree

---
+++ LeetCode/src/kuma/lc1261_Find_Elements_in_a_Contaminated_Binary_Tree/FindElements.java
@@ -0,0 +1,31 @@
+package kuma.lc1261_Find_Elements_in_a_Contaminated_Binary_Tree;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class FindElements {
+
+    TreeNode rootNode;
+    Set<Integer> set;
+
+    public FindElements(TreeNode root) {
+        rootNode = root;
+        set = new HashSet<Integer>();
+        setVal(rootNode, 0);
+    }
+
+    private void setVal(TreeNode node, int val) {
+        if (node != null) {
+            node.val = val;
+            set.add(val);
+            int base = 2 * val;
+            setVal(node.left, base + 1);
+            setVal(node.right, base + 2);
+        }
+    }
+
+    public boolean find(int target) {
+        return set.contains(target);
+    }
+
+}
