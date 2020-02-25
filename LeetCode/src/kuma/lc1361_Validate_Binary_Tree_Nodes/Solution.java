75c0dc60d719bd2040f1ec5863d71cdcbf06ba9f
xiong_yi <xiong_yi@192.168.128.101>
2020-02-25 15:50:48 +0900

    1361. Validate Binary Tree Nodes

---
+++ LeetCode/src/kuma/lc1361_Validate_Binary_Tree_Nodes/Solution.java
@@ -0,0 +1,29 @@
+package kuma.lc1361_Validate_Binary_Tree_Nodes;
+
+public class Solution {
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        int[] arr = new int[n];
+        for (int c : leftChild) {
+            if (c == 0)
+                return false;
+            if (c == -1)
+                continue;
+            if (arr[c] == 1)
+                return false;
+            arr[c] = 1;
+        }
+        for (int c : rightChild) {
+            if (c == 0)
+                return false;
+            if (c == -1)
+                continue;
+            if (arr[c] == 1)
+                return false;
+            arr[c] = 1;
+        }
+        for (int i = 1; i < n; i++)
+            if (arr[i] == 0)
+                return false;
+        return true;
+    }
+}
