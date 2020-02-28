b2e5b55caae77a079933d2a43ff2c35680319088
xiong_yi <xiong_yi@192.168.128.101>
2020-02-28 15:42:43 +0900

    46. Permutations

---
+++ LeetCode/src/kuma/lc0046_Permutations/Solution2.java
@@ -0,0 +1,32 @@
+package kuma.lc0046_Permutations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution2 {
+    public List<List<Integer>> permute(int[] nums) {
+        return helper(nums, 0);
+    }
+
+    public List<List<Integer>> helper(int[] nums, int index) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) {
+            List<Integer> list = new ArrayList<Integer>();
+            res.add(list);
+        } else if (index == nums.length - 1) {
+            List<Integer> list = new ArrayList<Integer>();
+            list.add(nums[index]);
+            res.add(list);
+        } else {
+            int num = nums[index];
+            for (List<Integer> list : helper(nums, index + 1)) {
+                for (int i = 0; i <= list.size(); i++) {
+                    List<Integer> l = new ArrayList<Integer>(list);
+                    l.add(i, num);
+                    res.add(l);
+                }
+            }
+        }
+        return res;
+    }
+}
