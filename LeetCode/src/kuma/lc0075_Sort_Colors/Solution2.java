2e6ddbe2abd9d336063b20c0c18cb3fe06dc8085
xiong_yi <xiong_yi@192.168.128.101>
2020-03-04 15:31:19 +0900

    75. Sort Colors

---
+++ LeetCode/src/kuma/lc0075_Sort_Colors/Solution2.java
@@ -0,0 +1,18 @@
+package kuma.lc0075_Sort_Colors;
+
+public class Solution2 {
+    public void sortColors(int[] nums) {
+        int[] arr = new int[3];
+        for (int num : nums)
+            arr[num]++;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i < arr[0])
+                nums[i] = 0;
+            else if (i < arr[0] + arr[1])
+                nums[i] = 1;
+            else
+                nums[i] = 2;
+        }
+    }
+}
