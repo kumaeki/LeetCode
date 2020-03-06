c270a68c4d5892127a7d6f5862e2364df0a77ce3
xiong_yi <xiong_yi@192.168.128.101>
2020-03-06 11:46:06 +0900

    78. Subsets

---
+++ LeetCode/src/kuma/lc0078_Subsets/Solution2.java
@@ -0,0 +1,27 @@
+package kuma.lc0078_Subsets;
+
+import java.util.*;
+
+public class Solution2 {
+    public List<List<Integer>> subsets(int[] nums) {
+        return helper(nums, 0);
+    }
+
+    public List<List<Integer>> helper(int[] nums, int index) {
+        List<List<Integer>> res;
+        if (index == nums.length) {
+            res = new ArrayList<List<Integer>>();
+            List<Integer> list = new ArrayList<Integer>();
+            res.add(list);
+        } else {
+            res = new ArrayList<List<Integer>>();
+            for (List<Integer> list : helper(nums, index + 1)) {
+                List<Integer> newlist = new ArrayList<Integer>(list);
+                newlist.add(nums[index]);
+                res.add(newlist);
+                res.add(list);
+            }
+        }
+        return res;
+    }
+}
