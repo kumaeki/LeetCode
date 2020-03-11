c9b61e54cab3ea9633ae9109003329a7c3db88b7
xiong_yi <xiong_yi@192.168.128.101>
2020-03-11 16:08:37 +0900

    104. Maximum Depth of Binary Tree

---
+++ LeetCode/src/kuma/lc0104_Maximum_Depth_of_Binary_Tree/TreeNode.java
@@ -0,0 +1,11 @@
+package kuma.lc0104_Maximum_Depth_of_Binary_Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
