ac14048e73a057ebfb8df4f2150a7967f2b168ac
xiong_yi <xiong_yi@192.168.128.101>
2020-03-12 13:47:32 +0900

    121. Best Time to Buy and Sell Stock

---
+++ LeetCode/src/kuma/lc0121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -0,0 +1,15 @@
+package kuma.lc0121_Best_Time_to_Buy_and_Sell_Stock;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0, res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int diff = prices[i] - prices[left];
+            if (diff >= 0)
+                res = Math.max(res, diff);
+            else
+                left = i;
+        }
+        return res;
+    }
+}
