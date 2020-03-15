94610b1154767e226ae9e8d88023548b7061659f
tedyi <tedyixiong@gmail.com>
2020-03-15 17:58:35 +0900

    1381. Design a Stack With Increment Operation

---
+++ LeetCode/src/kuma/lc1381_Design_a_Stack_With_Increment_Operation/CustomStack.java
@@ -0,0 +1,53 @@
+package kuma.lc1381_Design_a_Stack_With_Increment_Operation;
+
+import org.junit.jupiter.api.Test;
+
+class CustomStack {
+
+    int[] arr;
+    int cur;
+    int size;
+
+    public CustomStack(int maxSize) {
+        size = maxSize;
+        arr = new int[maxSize];
+        cur = 0;
+    }
+
+    public void push(int x) {
+        if (cur < size)
+            arr[cur++] = x;
+    }
+
+    public int pop() {
+        if (--cur >= 0)
+            return arr[cur];
+        else {
+            cur = 0;
+            return -1;
+        }
+            
+    }
+
+    public void increment(int k, int val) {
+        k = Math.min(k, Math.min(cur + 1, size));
+        for (int i = 0; i < k; i++)
+            arr[i] += val;
+    }
+
+    public static void main(String[] args) {
+        CustomStack obj = new CustomStack(3);
+        obj.push(1);
+        obj.push(2);
+        obj.pop();
+        obj.push(2);
+        obj.push(3);
+        obj.push(4);
+        obj.increment(5, 100);
+        obj.increment(2, 100);
+        obj.pop();
+        obj.pop();
+        obj.pop();
+        obj.pop();
+    }
+}
