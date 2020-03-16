f3e3f86117a608151718932e0d069abdb8069c1e
xiong_yi <xiong_yi@192.168.128.101>
2020-03-16 11:50:35 +0900

    1382. Balance a Binary Search Tree

---
+++ LeetCode/src/kuma/lc1382_Balance_a_Binary_Search_Tree/TreeNode.java
@@ -0,0 +1,11 @@
+package kuma.lc1382_Balance_a_Binary_Search_Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
