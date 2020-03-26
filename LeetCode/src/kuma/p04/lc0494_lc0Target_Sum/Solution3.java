51565440bd010ee1fd8f2ce8e5f11cd7b20cae0e
xiong_yi <xiong_yi@YI-XIONG>
2020-03-26 11:23:28 +0900

    494. Target Sum

---
+++ LeetCode/src/kuma/p04/lc0494_lc0Target_Sum/Solution3.java
@@ -0,0 +1,30 @@
+package kuma.p04.lc0494_lc0Target_Sum;
+
+import java.util.*;
+
+public class Solution3 {
+    // Runtime: 10 ms, faster than 73.35%
+    int[][] memo;
+
+    public int findTargetSumWays(int[] nums, int S) {
+        memo = new int[nums.length + 1][2001];
+        for (int[] m : memo)
+            Arrays.fill(m, Integer.MIN_VALUE);
+        return helper(nums, S, 0, 0);
+    }
+
+    private int helper(int[] nums, int S, int index, int sum) {
+        if (index == nums.length) {
+            return sum == S ? 1 : 0;
+        } else {
+            int res = memo[index][sum + 1000];
+            if (res != Integer.MIN_VALUE)
+                return res;
+            else {
+                res = helper(nums, S, index + 1, sum + nums[index]) + helper(nums, S, index + 1, sum - nums[index]);
+                memo[index][sum + 1000] = res;
+                return res;
+            }
+        }
+    }
+}
---
+++ LeetCode/src/kuma/p04/lc0494_lc0Target_Sum/Solution5.java
@@ -0,0 +1,23 @@
+package kuma.p04.lc0494_lc0Target_Sum;
+
+public class Solution5 {
+    // Runtime: 7 ms, faster than 83.03%
+    public int findTargetSumWays(int[] nums, int S) {
+        if (S > 1000)
+            return 0;
+        int[] dp = new int[2001];
+        dp[1000 + nums[0]]++;
+        dp[1000 - nums[0]]++;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int[] next = new int[2001];
+            for (int j = 0; j < 2001; j++) {
+                if (dp[j] > 0) {
+                    next[j + nums[i + 1]] += dp[j];
+                    next[j - nums[i + 1]] += dp[j];
+                }
+            }
+            dp = next;
+        }
+        return dp[S + 1000];
+    }
+}
