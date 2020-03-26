d504a511ad1242788f5a5d56b2cdd300833a1754
xiong_yi <xiong_yi@YI-XIONG>
2020-03-26 11:23:28 +0900

    494. Target Sum

---
+++ LeetCode/src/kuma/p04_lc0Target_Sum/Solution2.java
@@ -0,0 +1,15 @@
+package kuma.p04_lc0Target_Sum;
+
+public class Solution2 {
+    // Runtime: 519 ms, faster than 9.50% 
+    public int findTargetSumWays(int[] nums, int S) {
+        return helper(nums, S, 0, 0);
+    }
+    
+    private int helper(int[] nums, int S, int index, int sum){
+        if(index == nums.length)
+            return sum == S ? 1 : 0;
+        return helper(nums, S, index + 1, sum + nums[index]) 
+            + helper(nums, S, index + 1, sum - nums[index]);
+    }
+}
