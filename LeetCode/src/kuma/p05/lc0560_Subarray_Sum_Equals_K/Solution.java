479688714f4cc0b562a550706ca5ecfdb1c38ce8
xiong_yi <xiong_yi@192.168.128.100>
2020-04-03 11:30:15 +0900

    560. Subarray Sum Equals K

--- LeetCode/src/kuma/p05/lc0560_Subarray_Sum_Equals_K/Solution.java
+++ LeetCode/src/kuma/p05/lc0560_Subarray_Sum_Equals_K/Solution.java
@@ -7,13 +7,12 @@ import java.util.*;
 import org.junit.Test;
 
 public class Solution {
-    // Runtime: 13 ms, faster than 45.43%
+    // Runtime: 11 ms, faster than 96.02% of Java online submissions for Subarray
+    // Sum Equals K.
+    // Memory Usage: 40.5 MB, less than 30.43% of Java online submissions for
+    // Subarray Sum Equals K.
     public int subarraySum(int[] nums, int k) {
-        if (nums == null || nums.length == 0)
-            return 0;
-
-        int len = nums.length;
-        int count = 0, sum = 0;
+        int len = nums.length, count = 0, sum = 0;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         map.put(0, 1);
         for (int i = 0; i < len; i++) {
