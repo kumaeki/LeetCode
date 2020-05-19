855263028d73de25a3d43b61295334e6d3a08905
xiong_yi <xiong_yi@192.168.128.101>
2020-05-19 16:50:56 +0900

    220. Contains Duplicate III

---
+++ LeetCode/src/kuma/Test.java
@@ -0,0 +1,46 @@
+package kuma;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test {
+    public boolean checkInclusion(String s1, String s2) {
+        int n1 = s1.length(), n2 = s2.length();
+        if (n1 > n2)
+            return false;
+
+        int[] arr1 = new int[26];
+        for (char s : s1.toCharArray())
+            arr1[s - 'a']++;
+        int left = 0, right = 0, count = n1;
+        while(right < n2) {
+            if(arr1[s2.charAt(right++) - 'a']-- > 0)
+                count--;
+            if(count == 0)
+                return true;
+            if(right - left >= n1 && arr1[s2.charAt(left++)-'a']++ >= 0)
+                count++;
+        }
+        return false;
+    }
+
+
+    @org.junit.Test
+    public void simple1() {
+        assertEquals(true, new Test().checkInclusion("abc", "bcba"));
+    }
+
+    @org.junit.Test
+    public void simple2() {
+        assertEquals(true, new Test().checkInclusion("ab", "eidbaooo"));
+    }
+
+    @org.junit.Test
+    public void simple3() {
+        assertEquals(false, new Test().checkInclusion("ab", "eidbeaooo"));
+    }
+    
+    @org.junit.Test
+    public void simple4() {
+        assertEquals(false, new Test().checkInclusion("a", "o"));
+    }
+}
---
+++ LeetCode/src/kuma/p02/lc0220_Contains_Duplicate_III/Solution2.java
@@ -0,0 +1,46 @@
+package kuma.p02.lc0220_Contains_Duplicate_III;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.*;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution2 {
+    // Runtime: 19 ms, faster than 37.40% of Java online submissions for Contains
+    // Duplicate III.
+    // Memory Usage: 40.8 MB, less than 6.82% of Java online submissions for
+    // Contains Duplicate III.
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k < 1 || t < 0)
+            return false;
+        TreeSet<Long> tree = new TreeSet<Long>();
+        for (int i = 0; i < nums.length; i++) {
+            long num = nums[i];
+            Long pre = tree.floor(num), next = tree.ceiling(num);
+            if ((pre != null && num <= pre + t) || (next != null && next - t <= num))
+                return true;
+            tree.add(num);
+            if (i >= k)
+                tree.remove((long) nums[i - k]);
+        }
+        return false;
+    }
+
+    @Test
+    public void simple1() {
+        assertEquals(true, new Solution2().containsNearbyAlmostDuplicate(new int[] { 3, 6, 0, 2 }, 2, 2));
+    }
+
+    @Test
+    public void simple2() {
+        assertEquals(false, new Solution2().containsNearbyAlmostDuplicate(new int[] { -1, Integer.MAX_VALUE }, 1,
+                Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void simple3() {
+        assertEquals(false,
+                new Solution2().containsNearbyAlmostDuplicate(new int[] { 0, 10, 22, 15, 0, 5, 22, 12, 1, 5 }, 3, 3));
+    }
+}
