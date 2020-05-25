16d46e60eaba69a6d18ea30bddd6c598a82fe325
xiong_yi <xiong_yi@192.168.128.101>
2020-05-25 14:59:29 +0900

    282. Expression Add Operators

---
+++ LeetCode/src/kuma/p02/lc0282_Expression_Add_Operators/Solution.java
@@ -0,0 +1,47 @@
+package kuma.p02.lc0282_Expression_Add_Operators;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.*;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+    // Runtime: 97 ms, faster than 34.10% of Java online submissions for Expression
+    // Add Operators.
+    // Memory Usage: 40.2 MB, less than 83.78% of Java online submissions for
+    // Expression Add Operators.
+    public List<String> addOperators(String num, int target) {
+        List<String> list = new ArrayList<String>();
+        helper(list, num, target, 0, 0, 0, "");
+        return list;
+    }
+
+    private void helper(List<String> list, String num, int target, int index, long res, long pre, String path) {
+        if (index == num.length()) {
+            if (target == res)
+                list.add(path);
+            return;
+        }
+
+        for (int i = index; i < num.length(); i++) {
+            long cur = Long.valueOf(num.substring(index, i + 1));
+            if (index == 0)
+                helper(list, num, target, i + 1, res + cur, cur, String.valueOf(cur));
+            else {
+                helper(list, num, target, i + 1, res + cur, cur, path + "+" + cur);
+                helper(list, num, target, i + 1, res - cur, -cur, path + "-" + cur);
+                helper(list, num, target, i + 1, res - pre + pre * cur, pre * cur, path + "*" + cur);
+            }
+            if (num.charAt(i) == '0')
+                break;
+        }
+    }
+
+    @Test
+    public void simple1() {
+        String[] expecteds = new String[] { "2*3+2", "2+3*2" };
+        List<String> actuals = new Solution().addOperators("232", 8);
+        assertArrayEquals(expecteds, actuals.toArray(new String[2]));
+    }
+}
