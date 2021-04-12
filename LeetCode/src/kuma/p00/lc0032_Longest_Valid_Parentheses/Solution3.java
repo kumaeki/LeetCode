92645aa40cce0760e047adcf3dc0745f38a45257
Edward <35358166+kumaeki@users.noreply.github.com>
2021-04-12 22:16:38 +0900

    Update Solution3.java

--- LeetCode/src/kuma/p00/lc0032_Longest_Valid_Parentheses/Solution3.java
+++ LeetCode/src/kuma/p00/lc0032_Longest_Valid_Parentheses/Solution3.java
@@ -1,9 +1,5 @@
 package kuma.p00.lc0032_Longest_Valid_Parentheses;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.Test;
-
 public class Solution3 {
     public int longestValidParentheses(String s) {
         int len = s.length(), res = 0;
@@ -13,10 +9,11 @@ public class Solution3 {
             if (s.charAt(i) == ')') {
                 int pre = s.charAt(i - 1);
                 if (pre == '(')
-                    // dp[i] = (i - 2 >= 0 ? dp[i - 2] : 0) + 2;
+
                     dp[i] = (i > 1 ? dp[i - 2] : 0) + 2;
+                
                 else if (i - 1 - dp[i - 1] >= 0 && s.charAt(i - 1 - dp[i - 1]) == '(')
-                    // dp[i] = dp[i - 1] + 2 + (i - 2 - dp[i - 1] >= 0 ? dp[i - 2 - dp[i - 1]] : 0);
+
                     dp[i] = dp[i - 1] + 2 + (i - dp[i - 1] > 1 ? dp[i - dp[i - 1] - 2] : 0);
 
                 res = Math.max(res, dp[i]);
